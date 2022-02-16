069296a4d0fa592f3a07df4593ab939b3c661816
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-16 08:25:18 -0800

    Added js

--- js/data.js
+++ js/data.js
@@ -6,3 +6,16 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var values = localStorage.getItem('code-journal');
+if (values !== null) {
+  data = JSON.parse(values);
+}
+
+window.addEventListener('beforeunload', handlewindows);
+function handlewindows(event) {
+  var newObj = JSON.stringify(data);
+  localStorage.setItem('code-journal', newObj);
+  document.querySelector('img').reset();
+  document.querySelector('form').reset();
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
 /* global data */
 /* exported data */
+
+function handleInput(event) {
+  img.setAttribute('src', input.value);
+}
+var img = document.querySelector('img');
+var input = document.querySelector('#photoUrl');
+input.addEventListener('input', handleInput);
+
+var save = document.querySelector('form');
+function handleSubmit(event) {
+  var title = save.elements.title.value;
+  var photoUrl = save.elements.photoUrl.value;
+  var notes = save.elements.notes.value;
+  var Entry = {
+    title: title,
+    photo: photoUrl,
+    notes: notes,
+    nextEntryId: data.nextEntryId
+  };
+
+  data.nextEntryId++;
+  data.entries.unshift(Entry);
+}
+save.addEventListener('submit', handleSubmit);
