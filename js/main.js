5a28a2d0a5fb7dac85e47eb30ecc508e5231d322
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-16 11:19:10 -0800

    Made changes to main js

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ input.addEventListener('input', handleInput);
 
 var save = document.querySelector('form');
 function handleSubmit(event) {
+  event.preventDefault();
   var title = save.elements.title.value;
   var photoUrl = save.elements.photoUrl.value;
   var notes = save.elements.notes.value;
@@ -19,8 +20,9 @@ function handleSubmit(event) {
     notes: notes,
     nextEntryId: data.nextEntryId
   };
-  img.reset('images/placeholder-image-square.jpg');
   data.nextEntryId++;
   data.entries.unshift(Entry);
+  img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  save.reset();
 }
 save.addEventListener('submit', handleSubmit);
