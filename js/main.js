138b9eefd81d28821ea01649a1635874cdcb11a9
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-16 17:54:25 -0800

    use a for loop to create a DOM tree for each journal

--- js/main.js
+++ js/main.js
@@ -26,3 +26,37 @@ function handleSubmit(event) {
   save.reset();
 }
 save.addEventListener('submit', handleSubmit);
+
+function renderEntry(entry) {
+  var firstDiv = document.createElement('div');
+  firstDiv.setAttribute('class', 'row');
+
+  var colHalfdiv = document.createElement('div');
+  colHalfdiv.setAttribute('class', 'column-half');
+
+  var description = document.createElement('p');
+  description.textContent = entry.notes;
+
+  var image = document.createElement('img');
+  image.setAttribute('src', entry.photo);
+
+  var name = document.createElement('div');
+  name.setAttribute('class', 'column-half');
+
+  var heading = document.createElement('h2');
+  heading.textContent = entry.title;
+
+  firstDiv.appendChild(colHalfdiv);
+  colHalfdiv.appendChild(image);
+  colHalfdiv.appendChild(heading);
+  colHalfdiv.appendChild(description);
+  return firstDiv;
+
+}
+window.addEventListener('DOMcontentLoaded', renderEntry);
+var row = document.querySelector('entries');
+
+for (var i = 0; i < data.entry.length; i++) {
+  var entry = renderEntry(data.entries[i]);
+  row.appendChild(entry);
+}
