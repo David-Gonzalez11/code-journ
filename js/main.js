a94d2cabf4ee0c284971ee9a9968630ac617a6d1
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-02-26 16:48:32 -0800

    Made chnanges to 3rd to last task

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 /* global data */
 /* exported data */
-var save = document.querySelector('form');
+var $entryForm = document.querySelector('form');
 function handleInput(event) {
   img.setAttribute('src', input.value);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  var title = save.elements.title.value;
-  var photoUrl = save.elements.photoUrl.value;
-  var notes = save.elements.notes.value;
+  var title = $entryForm.elements.title.value;
+  var photoUrl = $entryForm.elements.photoUrl.value;
+  var notes = $entryForm.elements.notes.value;
 
   if (data.editing === null) {
     var Entry = {
@@ -25,60 +25,90 @@ function handleSubmit(event) {
     data.nextEntryId++;
     data.entries.unshift(Entry);
     img.setAttribute('src', 'images/placeholder-image-square.jpg');
-    save.reset();
-    save.classList.add('hidden');
-    $entriesView.className = ('');
+    // render the newly created entry
+    renderEntry(Entry);
   } else {
-    // get value from inputs  and replace the value in data.editing with the new input values
-    // go through data.entries to find entry with id that mactches data.editing.id
-    //
+    var existingEntryId = $entryForm.elements.existingEntryId.value;
+    var existingTitle = $entryForm.elements.title.value;
+    var existingPhotoUrl = $entryForm.elements.photoUrl.value;
+    var existingNotes = $entryForm.elements.notes.value;
+    var existingEntry = {
+      title: existingTitle,
+      photo: existingPhotoUrl,
+      notes: existingNotes,
+      id: existingEntryId
+    };
+    // console.log('existingEntryId', existingEntryId);
+    var indexToUpdate = data.entries.findIndex(entry => (Number(entry.id) === Number(existingEntryId)));
+    data.entries[indexToUpdate] = existingEntry;
+    data.editing = null;
+    replaceExisitngEntry(existingEntry);
   }
-  // just  adding new entry
-  // if not null then we want to update the new values to the data.entries
-  // after saving changes we want to set data.editing to null
-
-  //   var entryInput = {
-  //     title: title,
-  //     photoUrl: photoUrl,
-  //     notes: notes
-  //   };
-  // }
-  // var Entry = {
-  //   title: title,
-  //   photo: photoUrl,
-  //   notes: notes,
-  //   id: data.nextEntryId
-  // };
-
-  // when no entries at all nextEntryId is 1
-  // when new entry is created we give it id that is on dat.nextEntryID
-
-  data.nextEntryId++;
-  data.entries.unshift(Entry);
-  img.setAttribute('src', 'images/placeholder-image-square.jpg');
-  save.reset();
-  save.classList.add('hidden');
-  title = form.elements.title.value;
-  photoUrl = form.elements.photoUrl.value;
-  notes = form.elements.notes.value;
-  Entry = {
-    title: title,
-    photo: photoUrl,
-    notes: notes,
-    nextEntryId: data.nextEntryId
-  };
-  data.nextEntryId++;
-  data.entries.unshift(Entry);
-  img.setAttribute('src', 'images/placeholder-image-square.jpg');
-  form.reset();
-
+  $entryForm.reset();
   $entryForm.classList.add('hidden');
   $entriesView.className = ('');
 
 }
+function replaceExisitngEntry(entry) {
+  // grab eexisting element with entry id
+
+  var entryAttribute = '[data-entry-id="' + entry.id + '"]';
+  // console.log('attr', entryAttribute);
+  // console.log(document.querySelector(entryAttribute));
+  var oldListItem = document.querySelector(entryAttribute);
+  // try adding existing child to the list
+  // instead of append try and replace
+
+  var list = document.createElement('li');
+
+  var firstDiv = document.createElement('div');
+  firstDiv.setAttribute('class', 'row');
+
+  var colHalfdiv = document.createElement('div');
+  colHalfdiv.setAttribute('class', 'column-half');
+
+  var secondcolHalf = document.createElement('div');
+  secondcolHalf.setAttribute('class', 'column-half');
+
+  // var newdiv = document.createElement('div');
+  // newdiv.setAttribute('class', 'row')
+
+  var description = document.createElement('p');
+  description.textContent = entry.notes;
+
+  var image = document.createElement('img');
+  image.setAttribute('src', entry.photo);
+
+  var heading = document.createElement('h2');
+  heading.textContent = entry.title;
+
+  var button = document.createElement('button');
+  button.setAttribute('type', 'button');
+  button.className = 'edit-btn';
+
+  var editIcon = document.createElement('i');
+  editIcon.className = 'fas fa-user-edit pencil';
+  // var newcol = document.createElement('div');
+  // newcol.setAttribute('class', 'column-half n');
+
+  list.appendChild(firstDiv);
+  firstDiv.appendChild(colHalfdiv);
+  colHalfdiv.appendChild(image);
+  firstDiv.appendChild(secondcolHalf);
+  // secondcolHalf.appendChild(newcol);
+
+  secondcolHalf.appendChild(heading);
+  secondcolHalf.appendChild(description);
+  // secondcolHalf.appendChild(editIcon);
+  secondcolHalf.appendChild(button);
+  button.appendChild(editIcon);
+  // console.log('list:', list);
+  // replace old list item with new list item built above
+
+}
 
 function renderEntry(entry) {
-  var list = document.createElement('LI');
+  var list = document.createElement('li');
   list.setAttribute('data-entry-id', entry.id);
 
   var firstDiv = document.createElement('div');
@@ -104,6 +134,7 @@ function renderEntry(entry) {
 
   var button = document.createElement('button');
   button.setAttribute('type', 'button');
+  button.className = 'editBtn';
 
   var editIcon = document.createElement('i');
   editIcon.className = 'fas fa-user-edit pencil';
@@ -132,47 +163,50 @@ function showEntries(event) {
     $entryFormView.classList = '';
   } else {
     $entriesView.classList.add('hidden');
-    save.classList.remove('hidden');
+    $entryForm.classList.remove('hidden');
   }
 }
 // var parent = document.getElementsByClassName('.parent');
 
-// function editClick(event) {
-//   var toEdit = event.target.closest('li');
-//   var entryId = toEdit.getAttribute('data-entry-id');
-//   var entry = data.entries.find(entry => entry.id == entryId);
-//   if (event.target.tagName === 'I') {
-//     $entriesView.className = 'hidden';
-//     $entryFormView.className = '';
-//     data.editing = entry;
-//     var title = document.querySelector('#title');
-//     var notes = document.querySelector('#notes');
-//     var photoUrl = document.querySelector('#photoUrl');
-//     // data.editing
-//     title.value = (data.editing.title);
-//     notes.value = (data.editing.notes);
-//     photoUrl.value = (data.editing.photo);
-//   }
+function editClick(event) {
+  var toEdit = event.target.closest('li');
+  var entryId = toEdit.getAttribute('data-entry-id');
+  var entry = data.entries.find(entry => entry.id == entryId);
+  // console.log('entry', entry);
+  if (event.target.tagName === 'I') {
+    $entriesView.className = 'hidden';
+    $entryFormView.className = '';
+    data.editing = entry;
+    var title = document.querySelector('#title');
+    var notes = document.querySelector('#notes');
+    var photoUrl = document.querySelector('#photoUrl');
+    var existingEntryId = document.querySelector('#existingEntryId');
+    // data.editing
+    title.value = (data.editing.title);
+    notes.value = (data.editing.notes);
+    photoUrl.value = (data.editing.photo);
+    existingEntryId.value = (entryId);
+  }
 
-//   // hide the entries list and show the entry form when the edit icon is clicked
+  // hide the entries list and show the entry form when the edit icon is clicked
 
-// }
+}
 
 window.addEventListener('DOMContentLoaded', function (event) {
 
   // this was .parent before i changed it to .new//
-  var $entriesList = document.querySelector('.new');
-  // $entriesList.addEventListener('click', editClick);
+  var $entriesList = document.querySelector('.parent');
+  $entriesList.addEventListener('click', editClick);
   for (var i = 0; i < data.entries.length; i++) {
-    var entry = renderEntry(data.entries[i]);
-    $entriesList.appendChild(entry);
+    if (data.entries[i] !== null) {
+
+      var entry = renderEntry(data.entries[i]);
+      $entriesList.appendChild(entry);
+    }
   }
-  // console.log('entries:', $entriesList);
 
 });
 
-var $entryForm = document.querySelector('form');
-// console.log('new entry:', $entryForm);
 var $entryFormView = document.querySelector('div[data-view="entry-form"]');
 var $entriesView = document.querySelector("div[data-view='entries']");
 
@@ -183,7 +217,7 @@ $entryForm.classList.remove('hidden');
 
 function entryButton(event) {
   if (event.target.matches('#entries')) {
-    save.classList.add('hidden');
+    $entryForm.classList.add('hidden');
   } else {
     $entriesLink.className = '';
 
@@ -195,19 +229,7 @@ var input = document.querySelector('#photoUrl');
 
 input.addEventListener('input', handleInput);
 
-var form = document.querySelector('form');
-
-form.addEventListener('submit', handleSubmit);
-window.addEventListener('DOMContentLoaded', function (event) {
-  var $entriesList = document.querySelector('.new');
-  for (var i = 0; i < data.entries.length; i++) {
-    var entry = renderEntry(data.entries[i]);
-    $entriesList.appendChild(entry);
-  }
-});
-
-$entryForm = document.querySelector('form');
-$entryFormView = document.querySelector('div[data-view="entry-form"]');
+$entryForm.addEventListener('submit', handleSubmit);
 
 $entriesView.addEventListener('click', event => showEntries(event));
 
